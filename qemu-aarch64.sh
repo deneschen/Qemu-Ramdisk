99fa9b52a8c924b2bb5b3f615d231fef991aefda
Jun Chen <675816156@qq.com>
2018-12-28 07:20:15 -0800

    update qemu version 2018-12-28

--- qemu-aarch64.sh
+++ qemu-aarch64.sh
@@ -1,1 +1,1 @@
-~/bin/bin/qemu-system-aarch64 -machine virt -cpu cortex-a57 -machine type=virt -nographic -m 2048 -smp 4 -kernel ~/linux/arch/arm64/boot/Image --append "rdinit=/linuxrc console=ttyAMA0"
+${PWD}/bin/qemu-system-aarch64 -machine virt -cpu cortex-a57 -machine type=virt -nographic -m 2048 -smp 4 -kernel ~/linux/arch/arm64/boot/Image --append "rdinit=/linuxrc console=ttyAMA0"
