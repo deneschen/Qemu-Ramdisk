47d92ac6c0479e00a68830a0daee52d158d9d6ff
chenjun <675816156@qq.com>
2019-09-08 08:44:30 -0700

    uefi boot kernel on virt cortex-a57 platform.

--- qemu-aarch64-uefi.sh
+++ qemu-aarch64-uefi.sh
@@ -5,7 +5,7 @@ $(realpath ${exec_dir})/bin/qemu-system-aarch64 \
 	-machine type=virt \
 	-nographic -m 2048 \
 	-smp 4 \
+	-bios /home/jun/uefi/Build/ArmVirtQemu-AARCH64/DEBUG_GCC5/FV/QEMU_EFI.fd \
 	-kernel ~/linux/arch/arm64/boot/Image \
 	-initrd ${exec_dir}/ramdisk.img \
-	--append "rdinit=/linuxrc console=ttyAMA0" \
-	-s -S
+	--append "rdinit=/linuxrc console=ttyAMA0"
